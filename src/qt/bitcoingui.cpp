bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Qt: QComboBox::setVisible doesn't work in toolbars, so defer adding it at all until needed
Github-Pull: #8694
Rebased-From: d8da1836ed8face673decf20312bc1aa0c718aae

src/qt/bitcoingui.cpp
@@ -86,6 +86,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle *n
     progressBar(0),
     progressDialog(0),
     appMenuBar(0),
+    appToolBar(0),
     overviewAction(0),
     historyAction(0),
     quitAction(0),
@@ -437,6 +438,7 @@ void BitcoinGUI::createToolBars()
     if(walletFrame)
     {
         QToolBar *toolbar = addToolBar(tr("Tabs toolbar"));
+        appToolBar = toolbar;
         toolbar->setMovable(false);
         toolbar->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
         toolbar->addAction(overviewAction);
@@ -450,15 +452,7 @@ void BitcoinGUI::createToolBars()
         spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         toolbar->addWidget(spacer);
 
-        WalletSelectorLabel = new QLabel();
-        WalletSelectorLabel->setText(tr("Wallet:") + " ");
-        toolbar->addWidget(WalletSelectorLabel);
-        WalletSelectorLabel->setVisible(false);
         WalletSelector = new QComboBox();
-        toolbar->addWidget(WalletSelector);
-        WalletSelector->setVisible(false);
-        WalletSelectorLabel->setBuddy(WalletSelector);
-
         connect(WalletSelector, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(setCurrentWallet(const QString&)));
 #endif
     }
@@ -523,8 +517,11 @@ bool BitcoinGUI::addWallet(const QString& name, WalletModel *walletModel)
     setWalletActionsEnabled(true);
     WalletSelector->addItem(name);
     if (WalletSelector->count() == 2) {
-        WalletSelectorLabel->setVisible(true);
-        WalletSelector->setVisible(true);
+        WalletSelectorLabel = new QLabel();
+        WalletSelectorLabel->setText(tr("Wallet:") + " ");
+        WalletSelectorLabel->setBuddy(WalletSelector);
+        appToolBar->addWidget(WalletSelectorLabel);
+        appToolBar->addWidget(WalletSelector);
     }
     rpcConsole->addWallet(name, walletModel);
     return walletFrame->addWallet(name, walletModel);

equalishcoin/equalishcoind

src/qt/bitcoingui.cpp
@@ -91,6 +91,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *_platformStyle, const NetworkStyle *
     progressBar(0),
     progressDialog(0),
     appMenuBar(0),
+    appToolBar(0),
     overviewAction(0),
     historyAction(0),
     quitAction(0),
@@ -498,6 +499,7 @@ void BitcoinGUI::createToolBars()
         imageLogo->setPixmap(QPixmap(":/images/logo"));
         imageLogo->setObjectName("logo");
         QToolBar *toolbar = addToolBar(tr("Tabs toolbar"));
+        appToolBar = toolbar;
         toolbar->setMovable(false);
         toolbar->setToolButtonStyle(Qt::ToolButtonTextOnly);
         toolbar->addWidget(imageLogo);
@@ -516,15 +518,7 @@ void BitcoinGUI::createToolBars()
         spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         toolbar->addWidget(spacer);
 
-        m_wallet_selector_label = new QLabel();
-        m_wallet_selector_label->setText(tr("Wallet:") + " ");
-        toolbar->addWidget(m_wallet_selector_label);
-        m_wallet_selector_label->setVisible(false);
         m_wallet_selector = new QComboBox();
-        toolbar->addWidget(m_wallet_selector);
-        m_wallet_selector->setVisible(false);
-        m_wallet_selector_label->setBuddy(m_wallet_selector);
-
         connect(m_wallet_selector, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(setCurrentWallet(const QString&)));
 #endif
     }
@@ -600,8 +594,11 @@ bool BitcoinGUI::addWallet(const QString& name, WalletModel *walletModel)
     setWalletActionsEnabled(true);
     m_wallet_selector->addItem(name);
     if (m_wallet_selector->count() == 2) {
-        m_wallet_selector->setVisible(true);
-        m_wallet_selector->setVisible(true);
+        m_wallet_selector_label = new QLabel();
+        m_wallet_selector_label->setText(tr("Wallet:") + " ");
+        m_wallet_selector_label->setBuddy(m_wallet_selector);
+        appToolBar->addWidget(m_wallet_selector_label);
+        appToolBar->addWidget(m_wallet_selector);
     }
     rpcConsole->addWallet(name, walletModel);
     return walletFrame->addWallet(name, walletModel);