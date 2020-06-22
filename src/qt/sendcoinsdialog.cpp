bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin access from qt/guiutil.cpp

src/qt/sendcoinsdialog.cpp
@@ -212,7 +212,7 @@ void SendCoinsDialog::on_sendButton_clicked()
         SendCoinsEntry *entry = qobject_cast<SendCoinsEntry*>(ui->entries->itemAt(i)->widget());
         if(entry)
         {
-            if(entry->validate())
+            if(entry->validate(model->getIpcNode()))
             {
                 recipients.append(entry->getValue());
             }

equalishcoin/equalishcoind

src/qt/sendcoinsdialog.cpp
@@ -224,7 +224,7 @@ void SendCoinsDialog::on_sendButton_clicked()
         SendCoinsEntry *entry = qobject_cast<SendCoinsEntry*>(ui->entries->itemAt(i)->widget());
         if(entry)
         {
-            if(entry->validate())
+            if(entry->validate(model->node()))
             {
                 recipients.append(entry->getValue());
             }