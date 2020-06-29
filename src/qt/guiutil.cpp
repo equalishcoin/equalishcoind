hebasto/bitcoin-qtw

Fix autostart filenames on Linux

src/qt/guiutil.cpp
@@ -639,7 +639,7 @@ fs::path static GetAutostartFilePath()
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
         return GetAutostartDir() / "bitcoin.desktop";
-    return GetAutostartDir() / strprintf("bitcoin-%s.lnk", chain);
+    return GetAutostartDir() / strprintf("bitcoin-%s.desktop", chain);
 }
 
 bool GetStartOnSystemStartup()

equalishcoin/equalishcoind

src/qt/guiutil.cpp
@@ -631,7 +631,7 @@ fs::path static GetAutostartFilePath()
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
         return GetAutostartDir() / "peercoin.desktop";
-    return GetAutostartDir() / strprintf("peercoin-%s.lnk", chain);
+    return GetAutostartDir() / strprintf("peercoin-%s.desktop", chain);
 }
 
 bool GetStartOnSystemStartup()