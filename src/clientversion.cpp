bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

build: Rename BUILD_* macros and the code self-descriptive

src/clientversion.cpp
@@ -15,24 +15,13 @@
 const std::string CLIENT_NAME("Satoshi");
 
 
-/**
- * The following part of the code determines the CLIENT_BUILD variable.
- * Several mechanisms are used for this:
- * * first, if HAVE_BUILD_INFO is defined, include build.h, a file that is
- *   generated by the build environment, possibly containing the output
- *   of git-describe in a macro called BUILD_DESC
- * * secondly, if this is an exported version of the code, GIT_ARCHIVE will
- *   be defined (automatically using the export-subst git attribute), and
- *   GIT_COMMIT will contain the commit id.
- * * then, three options exist for determining CLIENT_BUILD:
- *   * if BUILD_DESC is defined, use that literally (output of git-describe)
- *   * if not, but GIT_COMMIT is defined, use v[maj].[min].[rev].[build]-g[commit]
- *   * otherwise, use v[maj].[min].[rev].[build]-unk
- */
-
-//! First, include build.h if requested
 #ifdef HAVE_BUILD_INFO
 #include <obj/build.h>
+// The <obj/build.h>, which is generated by the build environment (share/genbuild.sh),
+// could contain only one line of the following:
+//   - "#define BUILD_GIT_TAG ...", if the top commit is tagged
+//   - "#define BUILD_GIT_COMMIT ...", if the top commit is not tagged
+//   - "// No build information available", if proper git information is not available
 #endif
 
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
@@ -50,9 +39,11 @@ const std::string CLIENT_NAME("Satoshi");
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
 
-#ifndef BUILD_DESC
-#ifdef BUILD_SUFFIX
-#define BUILD_DESC BUILD_DESC_WITH_SUFFIX(CLIENT_VERSION_MAJOR, CLIENT_VERSION_MINOR, CLIENT_VERSION_REVISION, CLIENT_VERSION_BUILD, BUILD_SUFFIX)
+#ifdef BUILD_GIT_TAG
+#define BUILD_DESC BUILD_GIT_TAG
+#else
+#ifdef BUILD_GIT_COMMIT
+#define BUILD_DESC BUILD_DESC_WITH_SUFFIX(CLIENT_VERSION_MAJOR, CLIENT_VERSION_MINOR, CLIENT_VERSION_REVISION, CLIENT_VERSION_BUILD, BUILD_GIT_COMMIT)
 #elif defined(GIT_COMMIT_ID)
 #define BUILD_DESC BUILD_DESC_FROM_COMMIT(CLIENT_VERSION_MAJOR, CLIENT_VERSION_MINOR, CLIENT_VERSION_REVISION, CLIENT_VERSION_BUILD, GIT_COMMIT_ID)
 #else

equalishcoin/equalishcoind

src/clientversion.cpp
@@ -16,24 +16,13 @@ const std::string CLIENT_NAME("Satoshi");
 
 #define CLIENT_VERSION_SUFFIX " Tardigrade"
 
-/**
- * The following part of the code determines the CLIENT_BUILD variable.
- * Several mechanisms are used for this:
- * * first, if HAVE_BUILD_INFO is defined, include build.h, a file that is
- *   generated by the build environment, possibly containing the output
- *   of git-describe in a macro called BUILD_DESC
- * * secondly, if this is an exported version of the code, GIT_ARCHIVE will
- *   be defined (automatically using the export-subst git attribute), and
- *   GIT_COMMIT will contain the commit id.
- * * then, three options exist for determining CLIENT_BUILD:
- *   * if BUILD_DESC is defined, use that literally (output of git-describe)
- *   * if not, but GIT_COMMIT is defined, use v[maj].[min].[rev].[build]-g[commit]
- *   * otherwise, use v[maj].[min].[rev].[build]-unk
- */
-
-//! First, include build.h if requested
 #ifdef HAVE_BUILD_INFO
 #include <obj/build.h>
+// The <obj/build.h>, which is generated by the build environment (share/genbuild.sh),
+// could contain only one line of the following:
+//   - "#define BUILD_GIT_TAG ...", if the top commit is tagged
+//   - "#define BUILD_GIT_COMMIT ...", if the top commit is not tagged
+//   - "// No build information available", if proper git information is not available
 #endif
 
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
@@ -51,9 +40,11 @@ const std::string CLIENT_NAME("Satoshi");
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
 
-#ifndef BUILD_DESC
-#ifdef BUILD_SUFFIX
-#define BUILD_DESC BUILD_DESC_WITH_SUFFIX(PEERCOIN_VERSION_MAJOR, PEERCOIN_VERSION_MINOR, PEERCOIN_VERSION_REVISION, PEERCOIN_VERSION_BUILD, BUILD_SUFFIX)
+#ifdef BUILD_GIT_TAG
+#define BUILD_DESC BUILD_GIT_TAG
+#else
+#ifdef BUILD_GIT_COMMIT
+#define BUILD_DESC BUILD_DESC_WITH_SUFFIX(PEERCOIN_VERSION_MAJOR, PEERCOIN_VERSION_MINOR, PEERCOIN_VERSION_REVISION, PEERCOIN_VERSION_BUILD, BUILD_GIT_COMMIT)
 #elif defined(GIT_COMMIT_ID)
 #define BUILD_DESC BUILD_DESC_FROM_COMMIT(PEERCOIN_VERSION_MAJOR, PEERCOIN_VERSION_MINOR, PEERCOIN_VERSION_REVISION, PEERCOIN_VERSION_BUILD, GIT_COMMIT_ID)
 #else