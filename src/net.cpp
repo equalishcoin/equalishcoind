bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

net: Log network activity status change unconditionally

src/net.cpp
@@ -2251,14 +2251,12 @@ void Discover()
 
 void CConnman::SetNetworkActive(bool active)
 {
-    LogPrint(BCLog::NET, "SetNetworkActive: %s\n", active);
-
     if (fNetworkActive == active) {
         return;
     }
 
     fNetworkActive = active;
-
+    LogPrintf("%s: %s\n", __func__, fNetworkActive);
     uiInterface.NotifyNetworkActiveChanged(fNetworkActive);
 }
 

equalishcoin/equalishcoind

src/net.cpp
@@ -2255,7 +2255,7 @@ void Discover()
 
 void CConnman::SetNetworkActive(bool active)
 {
-    LogPrint(BCLog::NET, "SetNetworkActive: %s\n", active);
+    LogPrintf("%s: %s\n", __func__, active);
 
     if (fNetworkActive == active) {
         return;