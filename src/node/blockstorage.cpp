bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

style: Modernize range-based loops over m_block_index

src/node/blockstorage.cpp
@@ -221,8 +221,8 @@ bool BlockManager::LoadBlockIndex(
     // Calculate nChainWork
     std::vector<std::pair<int, CBlockIndex*>> vSortedByHeight;
     vSortedByHeight.reserve(m_block_index.size());
-    for (std::pair<const uint256, CBlockIndex>& item : m_block_index) {
-        CBlockIndex* pindex = &item.second;
+    for (auto& [_, block] : m_block_index) {
+        CBlockIndex* pindex = &block;
         vSortedByHeight.push_back(std::make_pair(pindex->nHeight, pindex));
     }
     sort(vSortedByHeight.begin(), vSortedByHeight.end());
@@ -379,8 +379,8 @@ bool BlockManager::LoadBlockIndexDB(ChainstateManager& chainman)
     // Check presence of blk files
     LogPrintf("Checking all blk files are present...\n");
     std::set<int> setBlkDataFiles;
-    for (const std::pair<const uint256, CBlockIndex>& item : m_block_index) {
-        const CBlockIndex* pindex = &item.second;
+    for (const auto& [_, block] : m_block_index) {
+        const CBlockIndex* pindex = &block;
         if (pindex->nStatus & BLOCK_HAVE_DATA) {
             setBlkDataFiles.insert(pindex->nFile);
         }

src/rpc/blockchain.cpp
@@ -1667,10 +1667,10 @@ static RPCHelpMan getchaintips()
     std::set<const CBlockIndex*> setOrphans;
     std::set<const CBlockIndex*> setPrevs;
 
-    for (const std::pair<const uint256, CBlockIndex>& item : chainman.BlockIndex()) {
-        if (!active_chain.Contains(&item.second)) {
-            setOrphans.insert(&item.second);
-            setPrevs.insert(item.second.pprev);
+    for (const auto& [_, block] : chainman.BlockIndex()) {
+        if (!active_chain.Contains(&block)) {
+            setOrphans.insert(&block);
+            setPrevs.insert(block.pprev);
         }
     }
 

equalishcoin/equalishcoind

src/node/blockstorage.cpp
@@ -112,8 +112,8 @@ bool BlockManager::LoadBlockIndex(
     // Calculate nChainTrust
     std::vector<std::pair<int, CBlockIndex*>> vSortedByHeight;
     vSortedByHeight.reserve(m_block_index.size());
-    for (std::pair<const uint256, CBlockIndex>& item : m_block_index) {
-        CBlockIndex* pindex = &item.second;
+    for (auto& [_, block_index] : m_block_index) {
+        CBlockIndex* pindex = &block_index;
         vSortedByHeight.push_back(std::make_pair(pindex->nHeight, pindex));
     }
     sort(vSortedByHeight.begin(), vSortedByHeight.end());
@@ -275,8 +275,8 @@ bool BlockManager::LoadBlockIndexDB(ChainstateManager& chainman)
     // Check presence of blk files
     LogPrintf("Checking all blk files are present...\n");
     std::set<int> setBlkDataFiles;
-    for (const std::pair<const uint256, CBlockIndex>& item : m_block_index) {
-        const CBlockIndex* pindex = &item.second;
+    for (const auto& [_, block_index] : m_block_index) {
+        const CBlockIndex* pindex = &block_index;
         if (pindex->nStatus & BLOCK_HAVE_DATA) {
             setBlkDataFiles.insert(pindex->nFile);
         }

src/rpc/blockchain.cpp
@@ -1598,10 +1598,10 @@ static RPCHelpMan getchaintips()
     std::set<const CBlockIndex*> setOrphans;
     std::set<const CBlockIndex*> setPrevs;
 
-    for (const std::pair<const uint256, CBlockIndex>& item : chainman.BlockIndex()) {
-        if (!active_chain.Contains(&item.second)) {
-            setOrphans.insert(&item.second);
-            setPrevs.insert(item.second.pprev);
+    for (const auto& [_, block_index] : chainman.BlockIndex()) {
+        if (!active_chain.Contains(&block_index)) {
+            setOrphans.insert(&block_index);
+            setPrevs.insert(block_index.pprev);
         }
     }
 