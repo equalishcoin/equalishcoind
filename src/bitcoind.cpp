63a5dcd2ac9a4d5428ab2a4b25b3d8a99d3964b5
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2026-05-30 21:12:05 -0400

    rebrand daemon help text and test docs

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -117,7 +117,7 @@ static bool AppInit(NodeContext& node, int argc, char* argv[])
 
     util::ThreadSetInternalName("init");
 
-    // If Qt is used, parameters/peercoin.conf are parsed in qt/bitcoin.cpp's main()
+    // If Qt is used, parameters/equalishcoin.conf are parsed in qt/bitcoin.cpp's main()
     ArgsManager& args = *Assert(node.args);
     SetupServerArgs(args);
     std::string error;
@@ -132,7 +132,7 @@ static bool AppInit(NodeContext& node, int argc, char* argv[])
         if (args.IsArgSet("-version")) {
             strUsage += FormatParagraph(LicenseInfo());
         } else {
-            strUsage += "\nUsage:  peercoind [options]                     Start " PACKAGE_NAME "\n"
+            strUsage += "\nUsage:  equalishcoind [options]                     Start " PACKAGE_NAME "\n"
                 "\n";
             strUsage += args.GetHelpMessage();
         }
@@ -159,7 +159,7 @@ static bool AppInit(NodeContext& node, int argc, char* argv[])
         // Error out when loose non-argument tokens are encountered on command line
         for (int i = 1; i < argc; i++) {
             if (!IsSwitchChar(argv[i][0])) {
-                return InitError(Untranslated(strprintf("Command line contains unexpected token '%s', see peercoind -h for a list of options.\n", argv[i])));
+                return InitError(Untranslated(strprintf("Command line contains unexpected token '%s', see equalishcoind -h for a list of options.\n", argv[i])));
             }
         }
 
